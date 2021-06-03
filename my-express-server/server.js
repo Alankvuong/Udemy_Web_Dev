6c55891f7284c4cf95e9833fd0e7184d590cb411
Alan Vuong <alankvuong@gmail.com>
2021-06-02 23:18:49 -0700

    Adding files for node.js and express.js section

---
+++ my-express-server/server.js
@@ -0,0 +1,29 @@
+// jslint esversion:6
+
+const express = require("express");
+const app = express();
+
+// --> / means to target the route(home) that we will respond to
+app.get("/", function(req, res)     // req = request, res = response
+{
+    res.send("<h1>Hello World</h1>");
+});
+
+app.get("/contact", function(req, res)
+{
+    res.send("<a href='mailto:alan92840@live.com'>Contact me at alan92840@live.com</a>");
+});
+
+app.get("/about", function(req, res)
+{
+    res.send("<h1>About me!</h1><p>Hello</p>");
+});
+
+app.get("/hobbies", function(req, res)
+{
+    res.send("I like playing basketball");
+})
+
+app.listen(3000, function(){
+    console.log("Server started on server 3000");
+});
