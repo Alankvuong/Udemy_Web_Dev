99b30912f452df25e215aa9ac7b3959e0d529a7f
Alankvuong <alankvuong@gmail.com>
2021-07-18 04:24:54 -0700

    Obtained and displayed the current condition as an icon

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -5,7 +5,7 @@ const app = express();
 
 
 app.get("/", function(req, res) {		// callback function
-	res.send("Hello");
+	//res.send("Hello");
 	const weatherAPI_url = "https://api.openweathermap.org/data/2.5/weather?q=Seattle&appid=df7631cdda75b7f31ed0716e20368687&units=imperial"
 
 	https.get(weatherAPI_url, function(response){
@@ -13,8 +13,19 @@ app.get("/", function(req, res) {		// callback function
 		
 		response.on("data", function(data){
 			const weatherData = JSON.parse(data);		// Converts hex to JSON
+			const temp = weatherData.main.temp;
+			const weatherDescription = weatherData.weather[0].description;
+			const weatherIcon =  weatherData.weather[0].icon
+			const iconUrl = "http://openweathermap.org/img/wn/" + weatherIcon + "@2x.png";
 			console.log("The temperature in Seattle is: " + weatherData.main.temp);
 			console.log("The current condition in Seattle is " + weatherData.weather[0].description);
+
+			res.write("<h1><u>Weather</u></h1>");
+			res.write("<h2>The temperature is Seattle is " + temp + " degrees Fahrenheit</h2>");
+			res.write("<h3>The current condition in Seattle is " + weatherDescription + ".</h3>");
+
+			res.write("<img src="+ iconUrl + ">");
+			res.send();
 		})		
 	})
 })			
