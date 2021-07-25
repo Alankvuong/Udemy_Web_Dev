940568b605294ef691be7af61879beb76f15d722
Alan Vuong <alankvuong@gmail.com>
2021-07-25 02:45:07 -0700

    Finished video setup for newsletter

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -4,15 +4,24 @@ const request = require("request");
 const https = require("https");
 
 const app = express();
+app.use(express.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 app.use(express.urlencoded({extended: true}));
 
 app.get("/", function(req, res) {
-    res.send("<p>Hello World</p>");
+    res.sendFile(__dirname + "/signup.html");
 })
 
 app.post("/", function(req, res){
-    res.send("<h1>Hello</h1>");
+    let firstName = req.body.firstName;     // Have to give "name=" in html input first
+    console.log(firstName);
+
+    let lastName = req.body.lastName;
+    console.log(lastName);
+
+    let email = req.body.email;
+    console.log(email);
 })
 
 app.listen(3000, function(){
