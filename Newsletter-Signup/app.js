2f5486763489718dcaa8c8f4436a7b4a81076350
Alankvuong <alankvuong@gmail.com>
2021-07-26 01:11:33 -0700

    Progressing along the newsletter module

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -5,7 +5,8 @@ const https = require("https");
 
 const app = express();
 app.use(express.urlencoded({extended: true}));
-app.use(express.static("public"));
+app.use(express.static("public"));                  // Have to have this in order to send static files (pictures/stylesheets)
+                                                            // "public" referes to the folder containing static files
 
 app.use(express.urlencoded({extended: true}));
 
@@ -14,16 +15,65 @@ app.get("/", function(req, res) {
 })
 
 app.post("/", function(req, res){
-    let firstName = req.body.firstName;     // Have to give "name=" in html input first
+    const firstName = req.body.firstName;     // Have to give "name=" in html input first
+
+    const lastName = req.body.lastName;
+    const email = req.body.email;
+
     console.log(firstName);
-
-    let lastName = req.body.lastName;
     console.log(lastName);
-
-    let email = req.body.email;
     console.log(email);
-})
+
+    let data = {        // Making an objeect b/c Mailchimp api requires object
+        members: [
+            {
+                email_address: email,
+                email_type: "html",
+                status: "subscribed",
+                merge_fields:{
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    }
+
+    const jsonData = JSON.stringify(data);        // Turns data above into json format
+    
+    const url = "https://us6.api.mailchimp.com/3.0/lists/b1305309d8";
+    const options = {
+        method: "POST",
+        auth: "alankvuong:03b302e98341d68f6565ea0d2464d92b-us6"
+    }
+
+    const request = https.request(url, options, function(response){
+        let statusCode = response.statusCode;
+        console.log("Status Code: " + statusCode);
+        if(statusCode === 200){
+            res.send("Thank you for subscribing to my newsletter!");
+        }
+        else{
+            res.send("You have encountered a " + statusCode + " error. Please try again later.");
+        }
+
+        if(statusCode === '200')
+        response.on("data", function(data){
+            console.log(JSON.parse(data));
+        })
+    })
+
+    request.write(jsonData);
+    request.end();
+
+});
 
 app.listen(3000, function(){
     console.log("Server is running at port 3000. http://localhost:3000");
 })
+
+// Mailchimp API Key
+// 03b302e98341d68f6565ea0d2464d92b-us6
+// 03b302e98341d68f6565ea0d2464d92b-us6
+
+// Audience/List ID
+// b1305309d8
