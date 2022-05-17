3bdfd6d6506f4b9230135594a15f81fa54939b99
Alan Vuong <alankvuong@gmail.com>
2022-05-17 00:48:24 -0700

    Updating files from last few lessons (ie EJS)

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -1,23 +1,58 @@
 //jslint esversion: 6;
 const express = require("express");
+const https = require("https");
+const bodyParser = require('body-parser');
+
+const date = require(__dirname + "/date.js");	// Requiring local module
 
 const app = express();
 
-app.use('view engine', 'ejs');
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({extended: true}));
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+
+var itemsArray = ["Buy food", "Cook Food", "Eat food"];		// Array with predefined todo list items
+var workList = [];
 
 app.get("/", function(req, res){
+	let day = date.getDate();		// Calling function getDate() using date module
 
-	var today = new Date();
-	var currentDay = today.getDay();
+	// Passes in two variables: one for day, one for items
+	res.render("list", {listTitle: day, newListItems: itemsArray});
+});
 
-	if (currentDay === 6 || currentDay === 0) {
-		res.sendFile(__dirname + "/weekend.html");
+app.get("/work", function(req, res) {
+	res.render("list", {listTitle: "Work List", newListItems: workList})
+});
 
+app.get("/about", function(req, res) {
+	res.render("about");
+});
+
+// POST REQUEST: Gets new user input upon Add Button pressed
+app.post("/", function(req, res) {
+	var item = req.body.newItem;		// Grabs the new item that was requested
+	if(req.body.list == "Work") {
+		workList.push(item);
+		res.redirect("/work");
 	} else {
-		res.sendFile(__dirname + "/weekday.html");
+		itemsArray.push(item);		// Places the item into an item array
+	
+		res.redirect("/");		// Redirects to the home page
 	}
 });
 
-app.listen(process.env.PORT || 3000, function () {
+app.post("/work", function(req, res) {
+	let item = req.body.newItem;
+	workList.push(item);
+
+	res.redirect("/work");
+})
+
+app.listen(3000, function () {
 	console.log("Server is running at port 3000. http://localhost:3000");
-  });
+});
