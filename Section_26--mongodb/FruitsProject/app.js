621ff247d090916d3dbbe778b14506292e0301f4
Alan Vuong <alan.vuong@iqair.com>
2022-06-01 17:33:18 -0700

    More work on FruitsProject mongodb

--- Section_26--mongodb/FruitsProject/app.js
+++ Section_26--mongodb/FruitsProject/app.js
@@ -1,10 +1,14 @@
 //jshint esversion:6
 
 const { MongoClient } = require("mongodb");
+const express = require("express");
+
+const app = express();
+const port = 3000;
 
 // Connection URI
 const uri =
-  "mongodb+srv://sample-hostname:27017/?maxPoolSize=20&w=majority";
+  "mongodb://localhost:27017";
 
 // Create a new MongoClient
 const client = new MongoClient(uri);
@@ -16,6 +20,10 @@ async function run() {
 
     // Establish and verify connection
     await client.db("admin").command({ ping: 1 });
+
+    // Make appropriate DB calls
+    await listDatabases(client);
+
     console.log("Connected successfully to server");
   } finally {
     // Ensures that the client will close when you finish/error
@@ -23,3 +31,21 @@ async function run() {
   }
 }
 run().catch(console.dir);
+
+async function listDatabases(client) {
+  databasesList = await client.db().admin().listDatabases();
+
+  console.log("Databases: ");
+  databasesList.databases.forEach(db => 
+    console.log(' - ${db.name}'));
+};
+
+
+// run on port 3000
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}. http://localhost:3000`)
+})
