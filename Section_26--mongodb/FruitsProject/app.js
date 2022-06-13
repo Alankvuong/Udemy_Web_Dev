b066348b71c2e7fd41f6a86ac2e90752a90b5260
Alan Vuong <alankvuong@gmail.com>
2022-06-13 00:03:59 -0700

    Native mongodb driver for nodejs

--- Section_26--mongodb/FruitsProject/app.js
+++ Section_26--mongodb/FruitsProject/app.js
@@ -1,6 +1,6 @@
 //jshint esversion:6
 
-const { MongoClient } = require("mongodb");
+const { MongoClient, CURSOR_FLAGS } = require("mongodb");
 const express = require("express");
 
 const app = express();
@@ -8,9 +8,9 @@ const port = 3000;
 
 // Connection URI
 const uri =
-  "mongodb://localhost:27017";
+  "mongodb://127.0.0.1:27017";
 
-// Create a new MongoClient
+// Create a new MongoClient 
 const client = new MongoClient(uri);
 
 async function run() {
@@ -19,12 +19,15 @@ async function run() {
     await client.connect();
 
     // Establish and verify connection
-    await client.db("admin").command({ ping: 1 });
+    await client.db("fruits").command({ ping: 1 });
+    console.log("Connected successfully to server");
 
     // Make appropriate DB calls
     await listDatabases(client);
-
-    console.log("Connected successfully to server");
+    await listCollections(client);
+    await insertMany(client);
+    await viewData(client);
+    
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
@@ -35,11 +38,47 @@ run().catch(console.dir);
 async function listDatabases(client) {
   databasesList = await client.db().admin().listDatabases();
 
-  console.log("Databases: ");
-  databasesList.databases.forEach(db => 
-    console.log(' - ${db.name}'));
-};
+  console.log("Databses:");
+  databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+}
 
+async function listCollections(client) {
+  collectionsList = await client.db("test").listCollections().toArray();
+
+  console.log("Collections:");
+  console.log(collectionsList);
+}
+
+async function insertMany(client) {
+  // Get the documents collection
+  const collection = client.db().collection('fruits');
+
+  // Insert documents
+  const docs = [
+    { name: "Apple", quantity: 5, review: "Great taste. Nice crunch!" },
+    { name: "Banana", quantity: 3, review: "Tasted okay. Kind of mushy."},
+    { name: "Grape", quantity: 10, review: "So juicy!" }
+  ];
+
+  const options = { ordered: true };
+
+  const insertManyresult = await collection.insertMany(docs, options);
+  let ids = insertManyresult.insertedIds;
+  console.log(`${insertManyresult.insertedCount} documents were inserted.`);
+  for (let id of Object.values(ids)) {
+    console.log(`Inserted a document with id ${id}`);
+  }
+}
+
+async function viewData(client) {
+  // Get the documents collection
+  const collection = client.db().collection('fruits');
+
+  const findResult = await collection.find();
+
+  console.log("Data from collection: ");
+  await findResult.forEach(console.dir);
+}
 
 // run on port 3000
 app.get('/', (req, res) => {
